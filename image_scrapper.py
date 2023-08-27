362d0c397e043541b4a5fcc6a2312ff9bad4828f
Wizzy <wizzy83@gmail.com>
2023-08-27 20:15:30 +0200

    image scrapper development

--- image_scrapper.py
+++ image_scrapper.py
@@ -6,15 +6,6 @@ from PIL import Image
 import time
 import os
 
-# setup paths
-os.getcwd()
-path = os.getcwd() + '\\data\\dogs\\'
-path_chromedriver = path + 'chromedriver-win64\\chromedriver.exe'
-
-# webdriver
-# wd = webdriver.Chrome(path_chromedriver)
-wd = webdriver.Chrome()
-
 # scroll down function
 def scroll_down(wd, delay):
 
@@ -23,10 +14,13 @@ def scroll_down(wd, delay):
 
     return
 
-def get_google_images(wd, delay, max_images):
+def get_google_images(delay, url, max_images):
 
-    url = 'https://www.google.com/search?q=dog+pictures&tbm=isch&ved=2ahUKEwiS3e-a4vWAAxVz-IsKHVOqCccQ2-cCegQIABAA&oq=dog+pictures&gs_lcp=CgNpbWcQAzIHCAAQigUQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABFAAWABguwZoAHAAeACAAVGIAVGSAQExmAEAqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=OIznZJLhKfPwrwTT1Ka4DA&bih=955&biw=1745'
+    wd = webdriver.Chrome()
     wd.get(url)
+
+    time.sleep(3)
+    wd.find_element(By.XPATH, "//span[contains(text(), 'Accept all')]").click()
 
     image_urls = set()
     skips = 0
@@ -43,7 +37,7 @@ def get_google_images(wd, delay, max_images):
             except:
                 continue
 
-            images = wd.find_elements(By.CLASS_NAME, 'n3VNCb')
+            images = wd.find_elements(By.CLASS_NAME, 'iPVvYb')
 
             for image in images:
                 if image.get_attribute('src') in image_urls:
@@ -55,9 +49,13 @@ def get_google_images(wd, delay, max_images):
                     image_urls.add(image.get_attribute('src'))
                     print(f'found {len(image_urls)}')
 
+    wd.quit()
+
     return image_urls
 
 def download_image(path, url, file_name):
+
+    wd = webdriver.Chrome()
 
     try:
         image_content = requests.get(url).content
@@ -79,112 +77,35 @@ def download_image(path, url, file_name):
 
     return
 
-# urls = get_google_images(wd, delay=1, max_images=max_images)
 
+# setup arguments
+path = os.getcwd() + '\\data\\dogs\\'
+url = 'https://www.google.com/search?q=dog+pictures&tbm=isch&ved=2ahUKEwiS3e-a4vWAAxVz-IsKHVOqCccQ2-cCegQIABAA&oq=dog+pictures&gs_lcp=CgNpbWcQAzIHCAAQigUQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABFAAWABguwZoAHAAeACAAVGIAVGSAQExmAEAqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=OIznZJLhKfPwrwTT1Ka4DA&bih=955&biw=1745'
+max_images = 100
+
+st = time.time()
+urls = get_google_images(delay=1, url=url, max_images=max_images)
+print(f'found {max_images} images in {(time.time() - st) / 60:.2f} min')
+
+st_global = time.time()
 for i, url in enumerate(urls):
-    download_images(path, url, str(i) + '.jpg', max_images)
 
-url = 'https://www.google.com/search?newwindow=1&sca_esv=559765737&sxsrf=AB5stBjz837urzE56V9afUz3Fimc1LsdZw:1692896310583&q=dog+pictures&tbm=isch&source=lnms&sa=X&ved=2ahUKEwiDhO-Z4vWAAxU_yLsIHQJxBoAQ0pQJegQIDhAB&biw=1745&bih=955&dpr=1.1'
-url = 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
-urls = [url]
-
-
-download_image(path, url, 'dog')
+    st_local = time.time()
+    download_image(path, url, f'dog_{i}')
+    print(f'downloaded image #{i} in {time.time() - st_local:.1f} s, total time {(time.time() - st_global) / 60:.2f} min')
 
 
 
+# time needed for 10k images (assuming it wont break)
+# finding 100 images 3.5 mins (need to fix load more images)
+# finding 10k images ~~ 6h
+# downloading an image ~ 8s
+# downloading 10k images ~~ 22 - 23h
+# total time around 30h ...
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-delay = 1
-max_images = 3
-url = 'https://www.google.com/search?q=dog+pictures&tbm=isch&ved=2ahUKEwiS3e-a4vWAAxVz-IsKHVOqCccQ2-cCegQIABAA&oq=dog+pictures&gs_lcp=CgNpbWcQAzIHCAAQigUQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABFAAWABguwZoAHAAeACAAVGIAVGSAQExmAEAqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=OIznZJLhKfPwrwTT1Ka4DA&bih=955&biw=1745'
-wd.get(url)
-
-image_urls = set()
-skips = 0
-
-
-
-while len(image_urls) + skips < max_images:
-    scroll_down(wd, delay)
-    thumbnails = wd.find_elements(By.CLASS_NAME, 'Q4LuWd')
-
-    for img in thumbnails[len(image_urls) + skips: max_images]:
-        try:
-            img.click()
-            time.sleep(delay)
-
-        except:
-            continue
-
-        images = wd.find_elements(By.CLASS_NAME, 'n3VNCb')
-
-        for image in images:
-            if image.get_attribute('src') in image_urls:
-                max_images += 1
-                skips += 1
-                break
-
-            if image.get_attribute('src') and 'http' in image.get_attribute('src'):
-                image_urls.add(image.get_attribute('src'))
-                print(f'found {len(image_urls)}')
-
-
-
-
-
-
-wd = webdriver.Chrome()
-
-
-
-url = 'https://www.google.com/search?q=dog+pictures&tbm=isch&ved=2ahUKEwiS3e-a4vWAAxVz-IsKHVOqCccQ2-cCegQIABAA&oq=dog+pictures&gs_lcp=CgNpbWcQAzIHCAAQigUQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABFAAWABguwZoAHAAeACAAVGIAVGSAQExmAEAqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=OIznZJLhKfPwrwTT1Ka4DA&bih=955&biw=1745'
-wd.get(url)
-# notification = wd.find_elements(By.CLASS_NAME, 'VfPpkd-vQzf8d')
-# notification = wd.find_elements(By.CLASS_NAME, 'vQzf8d')
-wd.switchTo().frame('Jh91Gc')
-wd.find_element(By.ID('id')).click()
-# notification = wd.find_element(By.CLASS_NAME, 'VfPpkd-Jh91Gc')
-# notification.click()
-
-
-
-
-wd.quit()
-
-
-len(image_urls) + skips < max_images
-scroll_down(wd, delay)
-thumbnails = wd.find_elements(By.CLASS_NAME, 'Q4LuWd')
-
-
-
-
-
-
-
-
-
+# fix show more results
+# save links after finding them
+# implement saving the state of downloading the links and resuming
 
 
 
