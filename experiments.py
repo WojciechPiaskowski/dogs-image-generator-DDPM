4c1691dbe02fa174d238d4beb8fc6cdd7ffbd80f
Wizzy <wizzy83@gmail.com>
2023-09-11 19:04:12 +0200

    cosine beta scheduler

--- experiments.py
+++ experiments.py
@@ -61,10 +61,21 @@ dl = load_transformed_dataset(path='data/cars')
 
 # forward process - adding noise / noise scheduler
 
-def linear_beta_scheduler(timesteps, start=0.0001, end=0.02):
-    out = torch.linspace(start, end, timesteps)
+def cosine_beta_schedule(timesteps, s=0.008):
+    """
+    cosine schedule as proposed in https://arxiv.org/abs/2102.09672
+    """
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps)
+    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * torch.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0.0001, 0.9999)
 
-    return out
+def linear_beta_schedule(timesteps):
+    beta_start = 0.0001
+    beta_end = 0.02
+    return torch.linspace(beta_start, beta_end, timesteps)
 
 
 # returns an index t of list given the batch dimension
@@ -95,7 +106,7 @@ def forward_diffusion_sample(x0, t, device='cuda'):
 
 # beta scheduler
 T = 300
-betas = linear_beta_scheduler(timesteps=T)
+betas = linear_beta_schedule(timesteps=T)
 
 # pre-calculate terms, alphas cumulative produts
 alphas = 1.0 - betas
